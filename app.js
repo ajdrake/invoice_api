4b69e5183d65c3280ce2cfca5c3225e08a452cc3
Aaron J Drake <aaronjdrake@adapt-llc.com>
2021-09-03 18:33:18 -0400

    added count

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const app = express()
-
+app.locals.count = 0;
 app.get('/', (req, res) => {
     res.send('<p>POST /invoice</p><p>GET /invoice/{id}</p><p>GET /count</p>');
 });
 
+app.post('/invoice/', (req, res) => {
+    console.log("an invoice posted");
+    app.locals.count++;
+})
+
 app.get('/invoice/:invoiceId', (req, res) => {
     var invoiceId = req.params.invoiceId;
+    app.locals.count++;
     console.log(invoiceId);
     res.send(invoiceId);
 })
 
+app.get('/count', (req, res) => {
+    res.send(app.locals.count.toString());
+})
 app.listen(3000, () => console.log('Application listening on port 3000!'))
 module.exports = app
