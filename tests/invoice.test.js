413b278c65ec605c372f8afb36c97b75ac59bdcb
Aaron J Drake <aaronjdrake@adapt-llc.com>
2021-09-03 21:03:58 -0400

    cleaned up lint errors

--- tests/invoice.test.js
+++ tests/invoice.test.js
@@ -1,5 +1,5 @@
+/* globals describe, it */
 const assert = require('assert');
-const expect = require('chai').expect
 const request = require('supertest');
 const app = require('../app')
 
@@ -14,24 +14,24 @@ describe('Unit testing the /invoice/{id} route', function() {
     });
 
     it('ensures the count is properly incremented', async () => {
-        let res = await request(app)
-          .post('/invoice').send({"date": "January 1, 2021", "date": "February 1, 2021", "items":["bananas", "oranges", "guava fruit"]});
+        await request(app)
+          .post('/invoice').send({"date": "January 1, 2021", "due_date": "February 1, 2021", "items":["bananas", "oranges", "guava fruit"]});
         request(app)
           .get('/count')
           .then(function(response){
               assert.equal(response.text, '1')
           })
 
-        res = await request(app)
-          .post('/invoice').send({"date": "January 2, 2021", "date": "February 2, 2021", "items":["blueberries", "apples", "pears"]});
+        await request(app)
+          .post('/invoice').send({"date": "January 2, 2021", "due_date": "February 2, 2021", "items":["blueberries", "apples", "pears"]});
         request(app)
           .get('/count')
           .then(function(response){
               assert.equal(response.text, '2')
           })
 
-        res = await request(app)
-          .post('/invoice').send({"date": "January 3, 2021", "date": "February 3, 2021", "items":["grapes", "watermellon", "grapefruit"]});
+        await request(app)
+          .post('/invoice').send({"date": "January 3, 2021", "due_date": "February 3, 2021", "items":["grapes", "watermellon", "grapefruit"]});
         
         return request(app)
           .get('/count')
